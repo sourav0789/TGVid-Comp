d1adf11e318cce04d158d0e4ef19ab3ac5211af4
sourav0789 <souravbosu3487@gmail.com>
2024-08-15 04:13:42 +0600

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("SnowEncoderBot")
+    return web.json_response("roserenamebot")
 
 
 async def web_server():
